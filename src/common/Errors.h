1ea5ededa2bfe4d8cc8fb0c73f79c21606e26c2d
Iván Montiel Cardona <ivan_montiel2013@hotmail.com>
2026-02-08 21:01:04 -0600

    feat(core): Introduce common error handling and refactor shared utilities

---
+++ src/common/Errors.h
@@ -0,0 +1,40 @@
+#ifndef ERRORS_H
+#define ERRORS_H
+
+#pragma once
+#include <stdexcept>
+#include <string>
+#include <utility>
+
+// Base class for all compilation errors
+class CompilerError : public std::runtime_error {
+public:
+    int line;
+    int column;
+    std::string type_name; // "SyntaxError", "IndentationError"
+
+    CompilerError(std::string name, const std::string &msg, const int l, const int c)
+        : std::runtime_error(msg), line(l), column(c), type_name(std::move(name)) {}
+};
+
+// Python: SyntaxError
+class SyntaxError : public CompilerError {
+public:
+    SyntaxError(const std::string& msg, const int l, const int c)
+        : CompilerError("SyntaxError", msg, l, c) {}
+};
+
+// Python: IndentationError
+class IndentationError : public CompilerError {
+public:
+    IndentationError(const std::string& msg, const int l, const int c)
+        : CompilerError("IndentationError", msg, l, c) {}
+};
+
+class LexicalError : public CompilerError {
+public:
+    LexicalError(const std::string& msg, const int l, const int c)
+        : CompilerError("LexicalError", msg, l, c) {}
+};
+
+#endif //ERRORS_H
--- src/shared/Types.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Ivan Montiel Cardona on 08/02/26.
-//
-
-#ifndef PYMCU_SUITE_TYPES_H
-#define PYMCU_SUITE_TYPES_H
-
-#endif //PYMCU_SUITE_TYPES_H
