7112403e039fc31da89bfa463f811bb3dbfee9f2
Iván Montiel Cardona <ivan_montiel2013@hotmail.com>
2026-02-15 17:40:18 -0600

    feat(lib): adds initial pymcu standard library
    Adds initial files for the PyMCU standard library, including HAL definitions, chip definitions, and type definitions.
    
    This library provides hardware abstractions and register definitions for various microcontrollers, licensed under GPLv3 with a commercial licensing option.

--- lib/src/pymcu/types.py
+++ lib/src/pymcu/types.py
@@ -1,3 +1,51 @@
+# -----------------------------------------------------------------------------
+# PyMCU Standard Library & HAL Definitions
+# Copyright (C) 2026 Ivan Montiel Cardona and the PyMCU Project Authors
+#
+# This file is part of the PyMCU Standard Library (pymcu-stdlib).
+#
+# pymcu-stdlib is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pymcu-stdlib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pymcu-stdlib.  If not, see <https://www.gnu.org/licenses/>.
+#
+# -----------------------------------------------------------------------------
+# NOTICE: STRICT COPYLEFT & STATIC LINKING
+# -----------------------------------------------------------------------------
+# This file contains hardware abstractions (HAL) and register definitions that
+# are statically linked (and/or inline expanded) into the final firmware binary by the pymcuc compiler.
+#
+# UNLIKE standard compiler libraries (e.g., GCC Runtime Library Exception),
+# NO EXCEPTION is granted for proprietary use.
+#
+# If you compile a program that imports this library, the resulting firmware
+# binary is considered a "derivative work" of this library under the GPLv3.
+# Therefore, any firmware linked against this library must also be released
+# under the terms of the GNU GPLv3.
+#
+# COMMERCIAL LICENSING:
+# If you wish to create proprietary (closed-source) firmware using PyMCU,
+# you must acquire a Commercial License from the copyright holder.
+#
+# For licensing inquiries, visit: https://pymcu.org/licensing
+# or contact: sales@pymcu.org
+#
+# -----------------------------------------------------------------------------
+# SAFETY WARNING / HIGH RISK ACTIVITIES:
+# THE SOFTWARE IS NOT DESIGNED, MANUFACTURED, OR INTENDED FOR USE IN HAZARDOUS
+# ENVIRONMENTS REQUIRING FAIL-SAFE PERFORMANCE, SUCH AS IN THE OPERATION OF
+# NUCLEAR FACILITIES, AIRCRAFT NAVIGATION OR COMMUNICATION SYSTEMS, AIR
+# TRAFFIC CONTROL, DIRECT LIFE SUPPORT MACHINES, OR WEAPONS SYSTEMS.
+# -----------------------------------------------------------------------------
+
 from typing import NewType, Generic, TypeVar
 
 T = TypeVar("T")
