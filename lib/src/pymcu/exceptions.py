1b1517ea3e348c34abdf740dacce9765b7cfa1bb
Iván Montiel Cardona <ivan_montiel2013@hotmail.com>
2026-06-02 18:25:58 -0600

    feat(stdlib): add pymcu.exceptions module with ValueError/TypeError/IndexError/KeyError codes

---
+++ lib/src/pymcu/exceptions.py
@@ -0,0 +1,6 @@
+# Exception type codes (uint8 constants).
+# Used with try/except and raise in PyMCU programs.
+ValueError: const = 1
+TypeError: const = 2
+IndexError: const = 3
+KeyError: const = 4
